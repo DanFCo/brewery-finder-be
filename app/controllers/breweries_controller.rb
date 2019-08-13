f8562ea8fe0a94900ca8d84c8c6449406772a150
DanFCo <danfcorreia@gmail.com>
2019-08-13 12:57:37 -0400

    basic functionality

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,2 +1,33 @@
 class BreweriesController < ApplicationController
+
+
+
+
+def index
+
+  breweries = Brewery.all
+  render json: breweries
+
 end
+
+def show
+
+brewery = Brewery.find(params[:id])
+
+render json: brewery
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+end #---------end of controller-----------------
